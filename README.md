ccb3f42c7b6f541c754950ba182b209aaad5d54d
nhuesman1043 <noah.huesman@gmail.com>
2024-02-09 22:32:23 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Project created to supplement my understanding of React. Also acts as a resource for anyone to learn more about me.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "brand-pruple": "#783F8E",
+      },
+    },
+  },
+  plugins: [],
+}
+
